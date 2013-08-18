cf1e7ef921f8dc0758ec3a450a8cf714ed271aee
Junya Hayashi <ledmonster@gmail.com>
2013-08-18 18:55:32 +0900

    Added models for publisher application

--- publisher/models.py
+++ publisher/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
+    name = models.CharField(max_length=32, primary_key=True)
+
+
+class Article(models.Model):
+    application = models.ForeignKey(Application)
+    is_active = models.BooleanField()
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Ad(models.Model):
+    is_active = models.BooleanField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
